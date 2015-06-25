5d0102819a1808017095b70c16c037a2690a376d
Tocino7 <dtocino@gmail.com>
2015-06-25 21:50:55 +0200

    Update aval.sql

--- resources/aval.sql
+++ resources/aval.sql
@@ -2,8 +2,8 @@
 -- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tiempo de generación: 22-06-2015 a las 21:46:44
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-06-2015 a las 21:50:17
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `oficinas` (
   `hora_apertura` time NOT NULL,
   `hora_cierre` time NOT NULL,
   `operador` varchar(20) COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `oficinas`
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `reservas` (
   `tarifa` int(11) NOT NULL,
   `momento_recogida` datetime NOT NULL,
   `momento_devolucion` datetime NOT NULL,
-  `oficina_devolucion` int(11) NOT NULL,
+  `oficina_devolucion` int(11) DEFAULT NULL,
   `cargado_cuenta` float NOT NULL,
   `estado` enum('prueba','','','') COLLATE utf8_spanish_ci NOT NULL,
   `NIF` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `reservas` (
   `extra_silla_elevador` tinyint(1) NOT NULL,
   `extra_portaesquis` tinyint(1) NOT NULL,
   `extra_cadenas` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `reservas`
@@ -81,7 +81,8 @@ CREATE TABLE IF NOT EXISTS `reservas` (
 
 INSERT INTO `reservas` (`ID`, `tarifa`, `momento_recogida`, `momento_devolucion`, `oficina_devolucion`, `cargado_cuenta`, `estado`, `NIF`, `nombre`, `apellidos`, `email`, `telefono1`, `telefono2`, `nacionalidad`, `fecha_nacimiento`, `extra_gps`, `extra_silla_bebe`, `extra_silla_elevador`, `extra_portaesquis`, `extra_cadenas`) VALUES
 (3, 3, '2015-06-02 00:01:00', '2015-06-02 23:06:00', 2, 11, 'prueba', '12345678B', 'Cambio', 'Hernando Sanchez', 'raul.dinformatica@gmail.com', 123456789, 123456789, 'Cambiada', '2015-01-31', 1, 1, 1, 0, 0),
-(4, 6, '2016-12-20 09:00:00', '2010-12-24 10:30:00', 2, 12.36, 'prueba', '05432212Q', 'Raul', 'Cobos Hernando', 'racbos@ucm.es', 913426512, 676543214, 'Francesa', '1992-12-11', 0, 1, 0, 1, 0);
+(4, 6, '2016-12-20 09:00:00', '2010-12-24 10:30:00', 2, 12.36, 'prueba', '05432212Q', 'Raul', 'Cobos Hernando', 'racbos@ucm.es', 913426512, 676543214, 'Francesa', '1992-12-11', 0, 1, 0, 1, 0),
+(7, 4, '0011-11-11 12:03:00', '1314-03-12 12:03:00', NULL, 0, 'prueba', '4234324324324', 'Daniel', 'Tocino Estrada', 'dtocino@gmail.com', 1312151543, 131231, 'peruana', '1342-02-13', 0, 1, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -100,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `tarifas` (
   `precio_tramo4` float NOT NULL,
   `km_max_diarios` float NOT NULL,
   `precio_km_extra` float NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `tarifas`
@@ -111,7 +112,8 @@ INSERT INTO `tarifas` (`ID`, `grupo`, `oficina`, `modulo_tramos`, `precio_tramo1
 (4, 'B', 2, 2, 2.1, 2.2, 2.3, 2.4, 2, 2),
 (5, 'C', 3, 1, 3.1, 3.2, 3.3, 3.4, 3, 3),
 (6, '1', 1, 2, 4.1, 4.2, 4.3, 4.4, 4, 4),
-(7, '2', 2, 2, 5.1, 5.2, 5.3, 5.4, 5, 5);
+(7, '2', 2, 2, 5.1, 5.2, 5.3, 5.4, 5, 5),
+(8, '3', 2, 2, 5.1, 5.2, 5.3, 5.4, 5, 5);
 
 -- --------------------------------------------------------
 
@@ -132,14 +134,16 @@ CREATE TABLE IF NOT EXISTS `vehiculo` (
   `alto` float NOT NULL,
   `ancho` float NOT NULL,
   `largo` float NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=687 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `vehiculo`
 --
 
 INSERT INTO `vehiculo` (`ID`, `tipo`, `grupo`, `modelo`, `puertas`, `plazas`, `radio`, `aire`, `metros_cubicos`, `alto`, `ancho`, `largo`) VALUES
-(2, 'industrial', '2', 'Renault Kangoo', 4, 5, 0, 1, 2.5, 3.6, 3.6, 6.2);
+(2, 'industrial', '2', 'Renault Kangoo', 4, 5, 0, 1, 2.5, 3.6, 3.6, 6.2),
+(456, 'industrial', 'B', 'Renault Kangoo', 4, 5, 0, 1, 2.5, 3.6, 3.6, 6.2),
+(686, 'turismo', '3', 'Renault Kangoo', 4, 5, 0, 1, 2.5, 3.6, 3.6, 6.2);
 
 --
 -- Índices para tablas volcadas
@@ -177,22 +181,22 @@ ALTER TABLE `vehiculo`
 -- AUTO_INCREMENT de la tabla `oficinas`
 --
 ALTER TABLE `oficinas`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT de la tabla `tarifas`
 --
 ALTER TABLE `tarifas`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT de la tabla `vehiculo`
 --
 ALTER TABLE `vehiculo`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=687;
 --
 -- Restricciones para tablas volcadas
 --
