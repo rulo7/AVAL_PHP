0345aab2e69dd74efa90aa3439c700990b3f4810
Tocino7 <dtocino@gmail.com>
2015-06-28 17:37:35 +0200

    Update viewTarificator3.php

--- presentation/tarificator/viewTarificator3.php
+++ presentation/tarificator/viewTarificator3.php
@@ -12,8 +12,8 @@ echo "<form method='post' action='tarificatorExecutor.php'>";
 
 
 echo "<p>Eliga las fechas de la reserva:</p>";
-echo "<p>Recogida (yyyy-mm-dd hh:mm): <input type='date' name='momento_recogida_date' id='id_momento_recogida_date'/> <input type='time' name='momento_recogida_time' id='id_momento_recogida_time'/></p>";
-echo "<p>Devolucion (yyyy-mm-dd hh:mm): <input type='date' name='momento_devolucion_date' id='id_momento_devolucion_date'/> <input type='time' name='momento_devolucion_time' id='id_momento_devolucion_time'/></p>";
+echo "<p>Recogida (yyyy-mm-dd hh:mm): <input type='date' name='momento_recogida_date' id='id_momento_recogida_date' required/> <input type='time' name='momento_recogida_time' id='id_momento_recogida_time' required/></p>";
+echo "<p>Devolucion (yyyy-mm-dd hh:mm): <input type='date' name='momento_devolucion_date' id='id_momento_devolucion_date' required/> <input type='time' name='momento_devolucion_time' id='id_momento_devolucion_time' required/></p>";
 
 $oficina = FrontController::getInstance()->execute(Entities::OFICINA, Operations::READ, $id_oficina);
 $devolucion= $oficina->getDevolucion_distinta_recogida();
@@ -21,7 +21,7 @@ if($devolucion)
 {
 echo "<div id='devolucion_distinta_recogida'>";
 $idOficinas = FrontController::getInstance()->execute(Entities::OFICINA, Operations::TOLIST, null);
-echo "<p>Oficina de devolucion: <select name='oficina_devolucion' id='oficina_devolucion'>";
+echo "<p>Oficina de devolucion: <select name='oficina_devolucion' id='oficina_devolucion' required>";
 foreach ($idOficinas as $idOficina) {
 
     $oficina = FrontController::getInstance()->execute(Entities::OFICINA, Operations::READ, $idOficina);
@@ -33,14 +33,14 @@ echo "</select></p>";
 echo "</div>";
 }
 echo "<p>Introduzca sus datos personales:</p>";
-echo "<p>NIF: <input type='text' name='NIF'/></p>";
-echo "<p>Nombre: <input type='text' name='nombre'/></p>";
-echo "<p>Apellidos: <input type='text' name='apellidos'/></p>";
-echo "<p>Email: <input type='email' name='email'/></p>";
-echo "<p>Telefono1: <input type='number' name='telefono1'/></p>";
-echo "<p>Telefono2: <input type='number' name='telefono2'/></p>";
-echo "<p>Nacionalidad: <input type='text' name='nacionalidad'/></p>";
-echo "<p>Fecha de nacimiento (yyyy-mm-dd): <input type='date' name='fecha_nacimiento'/></p>";
+echo "<p>NIF: <input type='text' name='NIF' required/></p>";
+echo "<p>Nombre: <input type='text' name='nombre' required/></p>";
+echo "<p>Apellidos: <input type='text' name='apellidos' required/></p>";
+echo "<p>Email: <input type='email' name='email' required/></p>";
+echo "<p>Telefono1: <input type='number' name='telefono1' required/></p>";
+echo "<p>Telefono2: <input type='number' name='telefono2' required/></p>";
+echo "<p>Nacionalidad: <input type='text' name='nacionalidad' required/></p>";
+echo "<p>Fecha de nacimiento (yyyy-mm-dd): <input type='date' name='fecha_nacimiento' required/> </p>";
 
 echo "<p>Seleccione los extras: </p>";
 echo "<p>Con extra GPS: <input type='checkbox' name='extra_gps'/></p>";
@@ -51,7 +51,9 @@ echo "<p>Con extra Cadenas: <input type='checkbox' name='extra_cadenas'/></p>";
 
 echo "<p><input type='hidden' name='estado' value='prueba'></p>";
 echo "<p><input type='hidden' name='tarifa' value='$id_tarifa'></p>";
+echo "<p><input type='hidden' name='oficina' value=$id_oficina></p>";
 
 echo "<p><input type='submit' value='Crear'></p>";
 echo "</form>";
+?>
 
